4c811f352e4586e95b3a88de705651f62a5b659a
wertedfrom <guerreronahuel@gmail.com>
2016-11-30 19:20:43 -0300

    Se realiza modificación menor en la migration de publicaciones para que acepte solo 50 caracteres en el titulo

--- database/migrations/2016_11_19_183539_create_publications_table.php
+++ database/migrations/2016_11_19_183539_create_publications_table.php
@@ -16,7 +16,7 @@ class CreatePublicationsTable extends Migration
         Schema::create('publications', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
-            $table->string('title');
+            $table->string('title',50);
             $table->string('description',500);
             $table->decimal('price');
             $table->integer('stock')->unsigned();
