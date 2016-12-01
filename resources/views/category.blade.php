d7cd871784d7f48380bed8a023e039d08f138901
Digital House <digitalhouse@DH-A3-06.digitalhouse.local>
2016-11-30 21:18:33 -0300

    se modifico el archivo css, cambio de color en los botones, redimencion del elemente publicacion en los listados, y cambio de 1 a 2 elementos para los dispositivos mobiles en los listados

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -9,7 +9,7 @@
             @foreach($publications as $publication)
                 {{--<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">--}}
                 {{--<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3 item">--}}
-                <div class="col-md-3">
+                <div class="col-xs-6 col-sm-4 col-md-3 ajuste">
                     <div class="thumbnail text-center" style="padding-bottom: 10px">
                         <a href="{{url("/publication/$publication->id")}}"><img src="{{asset('storage/'.$publication->url_image)}}" width="300" height="300" alt="{{$publication['title']}}"></a>
                             <h3 class="title">{{$publication['title']}}</h3>
